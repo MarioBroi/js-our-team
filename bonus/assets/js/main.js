06ea05b91ab5c6436544b54b47f2ba5b8dad741c
MarioBroi <inoix16@gmail.com>
2024-03-04 13:21:05 +0000

    add bonus folder

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,51 @@
+//console.log('test');
+
+//- Create a variable for the object array whit the different info inside
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+//console.log(team); //Array with the team elements
+
+const infoStrings = document.querySelector('.site-main');
+
+//- Create a console log with the following info inside a loop to print all the info one by one
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+    console.log(member.name, member.role, member.image);
+
+    //- Print the same informations obtained before on the DOM as strings
+    const markup = `<h3>${member.name}</h3><p>${member.role}</p><div>${member.image}</div>`
+
+    infoStrings.insertAdjacentHTML('beforeend', markup);
+
+};
---
+++ bonus/readme.md
@@ -0,0 +1,37 @@
+# Instructions 
+- Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
+- Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+
+- MILESTONE 1:
+    - stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+- MILESTONE 2:
+    - stampare le stesse informazioni su DOM sottoforma di stringhe
+
+- BONUS 1:
+    - trasformare la stringa foto in una immagine effettiva
+
+- BONUS 2:
+    - organizzare i singoli membri in card/schede
+
+
+## Steps
+
+- Create a variable for the object array whit the different info inside ✔
+
+- Create a console log with the following info inside a loop to print all the info one by one: ✔
+    - name
+    - role
+    - picture (string in this case)
+
+- Print the same informations obtained before on the DOM as strings ✔
+
+### Tools
+
+- const / let
+- array
+- objects
+- for cycle
+- console.log
+- query selector
+- innerHTML
